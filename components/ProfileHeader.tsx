1fbbd6bc0c4fed1377718db8e5abfa094d77fa6e
Tri-Hartono <trihartono293@gmail.com>
2023-04-30 20:52:34 +0700

    commit

--- components/ProfileHeader.tsx
+++ components/ProfileHeader.tsx
@@ -7,15 +7,15 @@ export default function ProfileHeader({ user }: Props) {
 
   const StatsItem = ({ value, label }: { value: number; label: string }) => {
     return (
-      <div className="flex flex-col items-center border border-zinc-200 p-4 rounded-lg">
-        <p className="text-2xl font-bold text-gray-700">{value}</p>
+      <div className="flex flex-col items-center border border-zinc-200 p-2 md:p-4 rounded-lg">
+        <p className="text-xl font-bold text-gray-700">{value}</p>
         <p className="font-semibold text-gray-500">{label}</p>
       </div>
     );
   };
   return (
     <div className="flex flex-col items-center space-x-5 justify-center">
-      <div className="flex space-x-4 w-full ">
+      <div className="flex items-center md:items-start space-x-4 w-full ">
         <Image alt="logo" src={user?.avatar_url} width={100} height={100} className="rounded-full border" />
         <div className="space-y-2">
           <p className="text-3xl md:text-5xl font-semibold">{user?.name}</p>
@@ -23,7 +23,7 @@ export default function ProfileHeader({ user }: Props) {
         </div>
       </div>
       <div>
-        <div className="inline-flex items-center  space-x-5 mt-5">
+        <div className="inline-flex items-center space-x-2 md:space-x-5 mt-5">
           <StatsItem value={user?.followers} label="Followers" />
           <StatsItem value={user?.following} label="Following" />
           <StatsItem value={user?.public_repos} label="Public Repos" />
